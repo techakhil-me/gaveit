51096efbdd7193f461b689780be951cc822b0a9d
TechAkhil <techakhilc47@gmail.com>
2021-08-23 23:59:07 +0530

    padding fixed MedBanner

--- src/components/MedBanner.jsx
+++ src/components/MedBanner.jsx
@@ -6,7 +6,7 @@ const MedBanner = () => {
   return (
     <Link
       to="/search/bottles"
-      className="cursor-pointer container relative mx-auto p-5 bg-green-100 flex justify-evenly items-center rounded-lg overflow-hidden"
+      className="cursor-pointer container relative mx-auto px-5 bg-green-100 flex justify-evenly items-center rounded-lg overflow-hidden"
     >
       <img src={Banner_two} alt="Logo" />
     </Link>
